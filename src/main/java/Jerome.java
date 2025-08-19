e457593e82ee00076a5b1a09d9d2de23c5955197
Jerome <jerome7van@gmail.com>
2025-08-19 21:55:58 +0800

    Added ability to mark and unmark tasks as done

--- src/main/java/Jerome.java
+++ src/main/java/Jerome.java
@@ -6,20 +6,32 @@ public class Jerome {
         String end = " Bye. Hope to see you again soon!";
         Scanner scn = new Scanner(System.in);
         System.out.println(start);
-        String tasks[] = new String[100];
+        Task tasks[] = new Task[100];
         int idx = 0;
         while (true) {
-            String echo = scn.nextLine();
-            if (echo.equals("bye")) {
+            String input = scn.nextLine();
+            String[] separated = input.split(" ");
+            if (input.equals("bye")) {
                 break;
-            } else if (echo.equals("list")) {
+            } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < idx; i++) {
-                    System.out.println(String.valueOf(i + 1) + ". " + tasks[i]);
+                    System.out.println(String.valueOf(i + 1) + ". " + "[" + tasks[i].getStatusIcon() + "] "+ tasks[i].getDescription());
                 }
+            } else if (separated[0].equals("mark")) {
+                int i = Integer.parseInt(separated[1]) - 1;
+                tasks[i].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + tasks[i].getStatusIcon() + "] "+ tasks[i].getDescription());
+            } else if (separated[0].equals("unmark")) {
+                int i = Integer.parseInt(separated[1]) - 1;
+                tasks[i].markAsNotDone();
+                System.out.println("Okay,  I've marked this task as not done yet:");
+                System.out.println("[" + tasks[i].getStatusIcon() + "] "+ tasks[i].getDescription());
             } else {
-                tasks[idx] = echo;
+                tasks[idx] = new Task(input);
                 idx += 1;
-                System.out.println("added: " + echo);
+                System.out.println("added: " + input);
             }
         }
         System.out.println(end);
