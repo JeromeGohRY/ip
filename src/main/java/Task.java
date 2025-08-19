dd5fef3325c13e1e5c8bb4f6c1c60a470679c4f3
Jerome <jerome7van@gmail.com>
2025-08-19 23:21:25 +0800

    Add javadoc comments for Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,7 @@
+/**
+ * Represents a task in the system.
+ * Holds task details like description and whether it is completed.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
@@ -7,6 +11,10 @@ public class Task {
         this.isDone = false;
     }
 
+    /**
+     * Provides string representation for whether task is completed
+     * @return corresponding symbol as a string depending on isDone
+     */
     public String getStatusIcon() {
         return isDone ? "X" : " ";
     }
@@ -16,10 +24,16 @@ public class Task {
         return "[" + getStatusIcon() + "] "+ this.description;
     }
 
+    /**
+     * Marks the task as done
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks the task as not done
+     */
     public void markAsNotDone() {
         this.isDone = false;
     }
