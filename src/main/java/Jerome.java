c8b781938ccac0f016229b72c77ee1b4b9190a1e
Jerome <jerome7van@gmail.com>
2025-08-19 15:14:23 +0800

    implemented echoing with termination on bye

--- src/main/java/Jerome.java
+++ src/main/java/Jerome.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class Jerome {
     public static void main(String[] args) {
-        String display = " Hello! I'm Jerome.\n"
-                + " What can I do for you? \n"
-                + " Bye. Hope to see you again soon!";
-        System.out.println(display);
+        String start = " Hello! I'm Jerome.\n"
+                + " What can I do for you? \n";
+        String end = " Bye. Hope to see you again soon!";
+        Scanner scn = new Scanner(System.in);
+        System.out.println(start);
+        while (true) {
+            String echo = scn.nextLine();
+            if (echo.equals("bye")) {
+                break;
+            } else{
+                System.out.println(echo);
+            }
+        }
+        System.out.println(end);
     }
 }
